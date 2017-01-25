47676bccf9cc045b63f0b231df1d35ad5be3110c
Yashaswini Amaresh <yashaswini.amaresh@gmail.com>
2017-01-24 20:47:04 -0800

    Cleaned the code by removing unnecessary comments

--- semi-supervised/ss_classify.py
+++ semi-supervised/ss_classify.py
@@ -9,22 +9,7 @@ def train_classifier(X, y):
 
 	######
 	from sklearn.model_selection import GridSearchCV
-	# param_grid = {'C': [0.001, 0.01, 0.1, 1, 10, 100, 1000] }
-	# clf = GridSearchCV(LogisticRegression(penalty='l1'), param_grid)
-	# clf1 = GridSearchCV(cv=None,
- #       estimator=LogisticRegression(intercept_scaling=1, dual=False, fit_intercept=True,
- #          penalty='l2', tol=0.0001),
- #       param_grid={'C': [0.001, 0.01, 0.1, 1, 10, 100, 1000], 'tol': [0.1, 0.01, 0.001, 0.0001, 0.00001, 0.0000001], 'solver' : ['newton-cg', 'lbfgs', 'liblinear', 'sag'] })
-
-	# clf.fit(X, y)
-	# clf1.fit(X, y)
-	# print "-----------------------------------------------------------------"
-	# print clf.best_params_
-	# print "-----------------------------------------------------------------"
-	# print clf1.best_params_
-	# print "-----------------------------------------------------------------"
-
-	######
+	
 	cls = LogisticRegression(penalty = 'l2', C = 10, tol=0.01)
 	cls.fit(X, y)
 	return cls
@@ -34,14 +19,7 @@ def train_classifier(X, y):
 def train_naive_bayes(X, y):
 	from sklearn.naive_bayes import MultinomialNB
 	from sklearn.model_selection import GridSearchCV
-	# clf1 = GridSearchCV(cv=None,
-	# 	estimator=MultinomialNB(),
-	# 	param_grid={'alpha': [0.001, 0.01, 0.1, 1, 10, 100, 1000]})
-	# clf1.fit(X, y)
-	# print "-----------------------------------------------------------------"
-	# print clf1.best_params_
-	# print "-----------------------------------------------------------------"
-
+	
 	cls = MultinomialNB(alpha = 0.01)
 	cls.fit(X,y)
 	return cls
@@ -51,15 +29,7 @@ def train_svm(X, y):
 	from sklearn.svm import LinearSVC
 	from sklearn.model_selection import GridSearchCV
 	from sklearn.calibration import CalibratedClassifierCV
-	# clf1 = GridSearchCV(cv=None,
-	# 	estimator=LinearSVC(intercept_scaling=1, dual=False, fit_intercept=True,
-	# 		penalty='l2', tol=0.0001),
-	# 	param_grid={'C': [0.001, 0.01, 0.1, 1, 10, 100, 1000], 'tol': [0.1, 0.01, 0.001, 0.0001, 0.00001, 0.0000001], 'penalty': ['l1', 'l2']})
-	# clf1.fit(X, y)
-	# print "-----------------------------------------------------------------"
-	# print clf1.best_params_
-	# print "-----------------------------------------------------------------"
-
+	
 	svm = LinearSVC()
 	cls = CalibratedClassifierCV(svm, method = 'isotonic')
 	cls.fit(X,y)
@@ -75,31 +45,12 @@ def addUnlabeled(unlabeled, X,y, cls_lr, cls_nb, cls_svm, speech):
 	pr_proba_nb = cls_nb.predict_proba(unlabeled.X)
 	pr_proba_svm = cls_svm.predict_proba(unlabeled.X)
 
-	# print cls_lr.classes_
-	# print "------------------------------" * 5
-	# print pr_proba_lr
-	# print "------------------------------" * 5
-	# print pr_proba_nb
-	# print "------------------------------" * 5
-
-	# print pr_proba_svm
-	# print "------------------------------" * 5
-
-	# print speech.le.classes_
-
+	
 	labels_lr = speech.le.inverse_transform(yp_lr)
 	labels_nb = speech.le.inverse_transform(yp_nb)
 	labels_svm = speech.le.inverse_transform(yp_svm)
 
 
-	# for i in range(len(labels_lr)):
-	# 	print str(labels_lr[i]) + " : " + str(pr_proba_lr[i])
-
-	# print "------------------------------" * 5
-	# print labels_nb
-
-	# f = open("data/labels.tsv", 'w')
-	# f.write("FileIndex,Category\n")
 	train_data, train_fnames, train_labels = getLabeledData()
 
 	for i in xrange(len(unlabeled.fnames)):
@@ -122,19 +73,7 @@ def addUnlabeled(unlabeled, X,y, cls_lr, cls_nb, cls_svm, speech):
 	# f.close()
 	from sklearn.feature_extraction.text import TfidfVectorizer
 
-	# speech.count_vect = TfidfVectorizer(analyzer = 'word', norm = 'l2', sublinear_tf = True)  
-	# speech.trainX = speech.count_vect.fit_transform(train_data)
-	# #print "Speech.trainX" + str(speech.trainX)
-	# from sklearn import preprocessing
-	# speech.le = preprocessing.LabelEncoder()
-	# speech.le.fit(speech.train_labels)
-	# speech.target_labels = speech.le.classes_
-	# speech.trainy = speech.le.transform(train_labels)
 	
-	# cls = train_classifier(speech.trainX, speech.trainy)
-	# print "After addin unlabeled data to the training set"
-	# evaluate(speech.trainX, speech.trainy, cls)
-
 	count_vect = TfidfVectorizer(analyzer = 'word', norm = 'l2', sublinear_tf = True, max_features = 7916)  
 	trainX = count_vect.fit_transform(train_data)
 	#print "Speech.trainX" + str(speech.trainX)
